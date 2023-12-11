38b074d2e3aa1016f2a06ae01d5340e06d570daf
DamianoL <damianolunardo@hotmail.it>
2023-12-11 17:09:01 +0100

    Add images, header/footer (draft)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,75 @@
+<footer class="bg-custom">
+<div class="container">
+    <div class="row">
+    <div class="col-2">
+        <ul class="footer-list">
+            DC COMICS
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+        </ul>
+        <ul class="footer-list">
+            SHOP
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+        </ul>
+    </div>
+    <div class="col-2">
+        <ul class="footer-list">
+            DC 
+            <li>Terms of Use</li>
+            <li>Privacy policy(New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+    <div class="col-2">
+        <ul class="footer-list">
+            SITES
+            <li>DC</li>
+            <li>Mad Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+        </ul>
+    </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <p>All Site Content TM and &copy; 2020 DC Entrtainment, unless otherwise nothed here. <br>
+    Cookies Settings</p>
+</div>
+
+<div class="bg-gray">
+    <div class="container custom-footer-bottom">
+        <div> 
+            <button class="button-footer">SIGN-UP NOW!</button>
+        </div>
+        <div>
+            <ul class="footer-list-bottom">
+                FOLLOW US
+                <li><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}"></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}"></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"</li>
+                <li><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}"</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header class="page-header bg-blue">
+    <div class="container top-header">
+        <ul class="top-header">
+            <li>DC POWER VISA</li>
+            <li>ADDITIONAL DC SITES</li>
+        </ul>
+    </div>
+<div>
+</header>
+
+<div class="container">
+    <ul class="bottom-header">
+        <li> <img src="{{ Vite::asset('resources/img/dc-logo.png') }}"></li>
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+        <li>Search</li>
+    </ul>
+</div>     
+
